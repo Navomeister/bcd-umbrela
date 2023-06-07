2cfc29f4c007eb0a8c10bb43ffa222033b2bc934
Nabo <110341514+Navomeister@users.noreply.github.com>
2023-06-07 17:25:18 -0300

    Add files via upload

--- Modelos/3 - Fisico/UmbrelaCorp_Fis.sql
+++ Modelos/3 - Fisico/UmbrelaCorp_Fis.sql
@@ -1,4 +1,4 @@
--- Geração de Modelo físico
+-- Geraï¿½ï¿½o de Modelo fï¿½sico
 -- Sql ANSI 2003 - brModelo.
 
 
@@ -6,23 +6,23 @@
 CREATE TABLE Store (
 storeId INT(10) PRIMARY KEY auto_increment,
 name VARCHAR(50),
-startTime VARCHAR(50),
+startTime DATETIME,
 customerGrade VARCHAR(50),
 streetAddr VARCHAR(50),
 city VARCHAR(50),
 province VARCHAR(50)
-)
+);
 
 CREATE TABLE Seller (
 sellerId INT(10) PRIMARY KEY auto_increment,
 user_id INT(10)
-)
+);
 
 CREATE TABLE User (
 userId INT(10) PRIMARY KEY auto_increment,
 name VARCHAR(50),
 phoneNum CHAR(11)
-)
+);
 
 CREATE TABLE Address (
 addrId INT(10) PRIMARY KEY auto_increment,
@@ -34,7 +34,7 @@ streetAddr VARCHAR(250),
 province VARCHAR(50),
 contactPhoneNumber CHAR(11),
 FOREIGN KEY(user_id) REFERENCES User (userId)
-)
+);
 
 CREATE TABLE Manage (
 sellerId INT(10),
@@ -42,54 +42,54 @@ storeId INT(10),
 setupTime DATETIME,
 FOREIGN KEY(sellerId) REFERENCES Seller (sellerId),
 FOREIGN KEY(storeId) REFERENCES Store (storeId)
-)
+);
 
-CREATE TABLE Order (
+CREATE TABLE `Order` (
 orderId INT(10) PRIMARY KEY auto_increment,
 creationTime DATETIME,
 paymentStatus VARCHAR(50),
 totalAmount INT(5)
-)
+);
 
 CREATE TABLE Contain (
 orderId INT(10),
 itemId INT(10),
 quantity INT(5),
-FOREIGN KEY(orderId) REFERENCES Order (orderId)
-)
+FOREIGN KEY(orderId) REFERENCES `Order` (orderId)
+);
 
 CREATE TABLE Order_Item (
 itemId INT(10) PRIMARY KEY auto_increment,
 product_id INT(10),
 price VARCHAR(50),
 creationTime DATETIME
-)
+);
 
 CREATE TABLE Deliver_To (
 addrId INT(10),
 orderId INT(10),
 timeDelivered DATETIME,
 FOREIGN KEY(addrId) REFERENCES Address (addrId),
-FOREIGN KEY(orderId) REFERENCES Order (orderId)
-)
+FOREIGN KEY(orderId) REFERENCES `Order` (orderId)
+);
 
 CREATE TABLE Payment (
 orderId INT(10),
 cardId INT(10),
 paytime DATETIME,
-FOREIGN KEY(orderId) REFERENCES Order (orderId)
-)
+FOREIGN KEY(orderId) REFERENCES `Order` (orderId)
+);
 
 CREATE TABLE Debit_Card (
 debitId INT(10) PRIMARY KEY auto_increment,
 card_id INT(10)
-)
+);
 
 CREATE TABLE Credit_Card (
 creditId INT(10) PRIMARY KEY auto_increment,
 card_id INT(10),
 organization VARCHAR(50)
-)
+);
 
 CREATE TABLE Bank_Card (
 cardId INT(10) PRIMARY KEY auto_increment,
@@ -98,14 +98,14 @@ user_id INT(10),
 bank VARCHAR(50),
 expiryDate DATE,
 FOREIGN KEY(user_id) REFERENCES User (userId)
-)
+);
 
 CREATE TABLE Save_To_Shopping_Cart (
 buyerId INT(10),
 productId INT(10),
 addTime DATETIME,
 quantity INT(10)
-)
+);
 
 CREATE TABLE Product (
 productId INT(10) PRIMARY KEY auto_increment,
@@ -119,19 +119,19 @@ colour VARCHAR(50),
 customerReview VARCHAR(50),
 modelNumber VARCHAR(50),
 FOREIGN KEY(store_id) REFERENCES Store (storeId)
-)
+);
 
 CREATE TABLE Buyer (
 buyerId INT(10) PRIMARY KEY auto_increment,
 user_id INT(10),
 FOREIGN KEY(user_id) REFERENCES User (userId)
-)
+);
 
-ALTER TABLE Seller ADD FOREIGN KEY(user_id) REFERENCES User (userId)
-ALTER TABLE Contain ADD FOREIGN KEY(itemId) REFERENCES Order_Item (itemId)
-ALTER TABLE Order_Item ADD FOREIGN KEY(product_id) REFERENCES Product (productId)
-ALTER TABLE Payment ADD FOREIGN KEY(cardId) REFERENCES Bank_Card (cardId)
-ALTER TABLE Debit_Card ADD FOREIGN KEY(card_id) REFERENCES Bank_Card (cardId)
-ALTER TABLE Credit_Card ADD FOREIGN KEY(card_id) REFERENCES Bank_Card (cardId)
-ALTER TABLE Save_To_Shopping_Cart ADD FOREIGN KEY(buyerId) REFERENCES Buyer (buyerId)
-ALTER TABLE Save_To_Shopping_Cart ADD FOREIGN KEY(productId) REFERENCES Product (productId)
+ALTER TABLE Seller ADD FOREIGN KEY(user_id) REFERENCES User (userId);
+ALTER TABLE Contain ADD FOREIGN KEY(itemId) REFERENCES Order_Item (itemId);
+ALTER TABLE Order_Item ADD FOREIGN KEY(product_id) REFERENCES Product (productId);
+ALTER TABLE Payment ADD FOREIGN KEY(cardId) REFERENCES Bank_Card (cardId);
+ALTER TABLE Debit_Card ADD FOREIGN KEY(card_id) REFERENCES Bank_Card (cardId);
+ALTER TABLE Credit_Card ADD FOREIGN KEY(card_id) REFERENCES Bank_Card (cardId);
+ALTER TABLE Save_To_Shopping_Cart ADD FOREIGN KEY(buyerId) REFERENCES Buyer (buyerId);
+ALTER TABLE Save_To_Shopping_Cart ADD FOREIGN KEY(productId) REFERENCES Product (productId);
